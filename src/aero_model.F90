f2086441394a85bb82d30f481ce650f5040ffc96
Ove <ovehaugv@outlook.com>
2025-09-08 13:51:20 +0200

    Add namelist reading subroutines for aerosol parameters
    The changes are made to add additional namelist parameters  that can be perturbed in a PPE setting.
    
    New namelist parameters implemented:
    - dst_density
    - dst_solfact
    - sol_facti_cloud_borne
    - sol_factb_interstitial
    - sol_factic_interstitial
    - lifeCycleNumberMedianRadius
    - lifeCycleSigma
    
    Moved the sol_facti_cloud_borne, sol_factic_interstital etc.. to the
    oslo_aero_share_readnl routine from the aero_model_readnl routine. Added
    one nml parameter from NorESM2.3 "modal_aero_wateruptake".
    
    Lastly added new nmlparameter "emis_fact_in_coarse_mode" for scaling the fraction of dust emission
    put onto the coarse model. The ammount put on the fine dust mode is
    determined by the 1-coarse model fraction.

--- src/aero_model.F90
+++ src/aero_model.F90
@@ -57,10 +57,13 @@ module aero_model
   use oslo_aero_ocean,          only: oslo_aero_ocean_init, oslo_aero_dms_emis
   use oslo_aero_sox_cldaero,    only: sox_cldaero_init
   use oslo_aero_microp,         only: oslo_aero_microp_readnl
+  use oslo_aero_share,          only: oslo_aero_share_readnl
   use oslo_aero_sw_tables,      only: initopt
   use oslo_aero_aerodry_tables, only: initdry
   use oslo_aero_aerocom_tables, only: initaeropt
   use oslo_aero_logn_tables,    only: initlogn
+
+  use modal_aero_wateruptake, only: modal_strat_sulfate
 
   implicit none
   private
@@ -100,19 +103,13 @@ module aero_model
   integer :: ndx_soa_sv = -1 ! for surf_area_dens
   logical :: convproc_do_aer = .false.
 
-  ! Namelist variables
-  real(r8) :: sol_facti_cloud_borne   = 1._r8
-  real(r8) :: sol_factb_interstitial  = 0.1_r8
-  real(r8) :: sol_factic_interstitial = 0.4_r8
-
 !=============================================================================
 contains
 !=============================================================================
 
   subroutine aero_model_readnl(nlfilename)
-    ! OSLO_AERO begin
+
     use oslo_aero_dust,   only: oslo_aero_dust_readnl
-    ! OSLO_AERO end
 
     ! read aerosol namelist options
 
@@ -122,12 +119,12 @@ contains
     integer :: unitn, ierr
     character(len=*), parameter :: subname = 'aero_model_readnl'
 
-    namelist /aerosol_nl/ sol_facti_cloud_borne, sol_factb_interstitial, sol_factic_interstitial
+    namelist /aerosol_nl/ modal_strat_sulfate
     !-----------------------------------------------------------------------------
 
     ! Read namelist
     if (masterproc) then
-       open(newunit=unitn, file=trim(nlfilename), status='old' )
+       open(newunit=unitn, file=trim(nlfilename), status='old')
        call find_group_name(unitn, 'aerosol_nl', status=ierr)
        if (ierr == 0) then
           read(unitn, aerosol_nl, iostat=ierr)
@@ -137,18 +134,13 @@ contains
        end if
        close(unitn)
     end if
-    call mpi_bcast(sol_facti_cloud_borne, 1, mpi_real8, mstrid, mpicom, ierr)
-    if (ierr /= mpi_success) call endrun(subname//" mpi_bcast: sol_facti_cloud_borne")
-    call mpi_bcast(sol_factb_interstitial, 1, mpi_real8, mstrid, mpicom, ierr)
-    if (ierr /= mpi_success) call endrun(subname//" mpi_bcast: sol_factb_interstitial")
-    call mpi_bcast(sol_factic_interstitial, 1, mpi_real8, mstrid, mpicom, ierr)
-    if (ierr /= mpi_success) call endrun(subname//" mpi_bcast: sol_factic_interstitial")
+    call mpi_bcast(modal_strat_sulfate, 1, mpi_logical, mstrid, mpicom, ierr)
+    if (ierr /= mpi_success) call endrun(subname//" mpi_bcast: modal_strat_sulfate")
 
+    call oslo_aero_share_readnl(nlfilename)
     call oslo_aero_ctl_readnl(nlfilename)
     call oslo_aero_microp_readnl(nlfilename)
-   ! OSLO_AERO begin
-   call oslo_aero_dust_readnl(nlfilename)
-   ! OSLO_AERO end
+    call oslo_aero_dust_readnl(nlfilename)
 
   end subroutine aero_model_readnl
 
